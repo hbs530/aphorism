407f722b297c6f7fea6643e44d66453322d92543
Hyun Beomseok <hbs530@naver.com>
2023-11-05 14:42:04 +0900

    stage 1 complete

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -2,15 +2,20 @@ package com.ll;
 
 import java.util.Scanner;
 
-class App {
-    static void run() {
-        System.out.println("== 명언 앱 ==");
+public class App {
+    public void run() {
+        while(true) {
+            System.out.println("== 명언 앱 ==");
+            System.out.print("명령) ");
 
-        System.out.print("명령) ");
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
 
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
+            if (cmd.equals("종료")){
+                break;
+            }
 
-        System.out.printf("입력하신 명령 : %s\n", cmd);
+            System.out.printf("입력받은 명령어 : %s\n", cmd);
+        }
     }
 }
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,7 +2,7 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-        App.run();
+        new App().run();
     }
 }
 
