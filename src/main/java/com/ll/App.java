d7010613757eec92ae8f59e4dc6aa439818864cb
Hyun Beomseok <hbs530@naver.com>
2023-11-05 21:29:25 +0900

    stage 6

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -29,6 +29,8 @@ public class App {
                 actionWrite();
             } else if (cmd.equals("목록")){
                 actionList();
+            } else if (cmd.startsWith("삭제?")){
+                actionRemove(cmd);
             }
         }
     }
@@ -61,5 +63,12 @@ public class App {
             Quotation quotation = quotations.get(i);
             System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
         }
+    }
+
+    void actionRemove(String cmd){
+        String idStr = cmd.replace("삭제?id=", "");
+        int id = Integer.parseInt(idStr);
+
+        System.out.printf("%d번 명언을 삭제합니다.\n", id);
     }
 }
